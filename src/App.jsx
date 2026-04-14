e17bd568a7224a22c397966eb229d4237ed03352
Jubaer Rahman <coursesmy3@gmail.com>
2026-04-14 22:23:17 +0600

    expoimpo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
 import { useState } from 'react'
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Stats from "./components/Stats";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import Steps from "./components/Steps";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+
 
 import './App.css'
 
